8a1ab323b986d7d4c840f62a0655ee1d4754a1ec
LiaContreras <liakiki166@gmail.com>
2025-02-14 13:37:45 -0600

    Starling TabBarView

---
+++ donut_app_2c_contreras-main/lib/pages/home_page.dart
@@ -0,0 +1,84 @@
+import 'package:donut_app_2c_contreras/tabs/burger_tab.dart';
+import 'package:donut_app_2c_contreras/tabs/donut_tab.dart';
+import 'package:donut_app_2c_contreras/tabs/pancakes_tab.dart';
+import 'package:donut_app_2c_contreras/tabs/pizza_tab.dart';
+import 'package:donut_app_2c_contreras/tabs/smoothie_tab.dart';
+import 'package:donut_app_2c_contreras/utils/my_tab.dart';
+import 'package:flutter/material.dart';
+ 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+ 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Widget> myTabs = [const 
+  MyTab(iconPath: 'lib/icons/donut.png'),
+  MyTab(iconPath: 'lib/icons/burger.png'),
+  MyTab(iconPath: 'lib/icons/smoothie.png'),
+  MyTab(iconPath: 'lib/icons/pancakes.png'),
+  MyTab(iconPath: 'lib/icons/pizza.png'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor:Colors.transparent,
+          leading: Icon(Icons.menu,
+          color:Colors.grey[800]
+          ),
+          actions: const [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            //Tesxto principal
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+              child: Row(
+                children: [
+                  Text("I want to ", style: TextStyle(fontSize: 32 ),), 
+                  Text("Eat", 
+                  style: TextStyle(
+                    //Tamaño de letra
+                    fontSize: 32,
+              
+                    //Negritas
+                    fontWeight: FontWeight.bold,
+              
+                    //Subrayado 
+                    decoration: TextDecoration.underline) ,)
+                ],
+              ),
+            ),
+            
+      
+            //Pestañas(Tapbar)
+            TabBar(tabs: myTabs),
+      
+            //Contenido de pestañas (TabBarView)
+            Expanded(
+              child: TabBarView(children: [
+                DonutTab(),
+                BurgerTab(),
+                SmoothieTab(),
+                PancakesTab(),
+                PizzaTab()
+              ]),
+            )
+            //Carrito(Cart)
+          ]
+        )
+      ),
+    );
+  }
+}
